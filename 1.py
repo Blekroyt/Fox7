5e7a828fc5293e17441f43667852045a90f23b4f
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-12-25 15:06:40 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,92 +4,110 @@ import argparse
 import json
 import os.path
 import sys
+from datetime import date
 
 
-def add_shop(list_shop, name_shop, name_product, prise):
+def add_worker(staff, name, post, year):
     """
-    Добавить данные магазина.
+    Добавить данные о работнике.
     """
-    list_shop.append(
+    staff.append(
         {
-            "name_shop": name_shop,
-            "name_product": name_product,
-            "prise": prise
+            "name": name,
+            "post": post,
+            "year": year
         }
     )
-    return list_shop
+    return staff
 
 
-def display_shop(list_shop):
+def display_workers(staff):
     """
-    Отобразить список магазинов.
+    Отобразить список работников.
     """
-    if list_shop:
+    # Проверить, что список работников не пуст.
+    if staff:
         # Заголовок таблицы.
         line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-            '-' * 6,
-            '-' * 20,
+            '-' * 4,
             '-' * 30,
-            '-' * 20
+            '-' * 20,
+            '-' * 8
         )
         print(line)
         print(
-            '| {:^6} | {:^20} | {:^30} | {:^20} |'.format(
+            '| {:^4} | {:^30} | {:^20} | {:^8} |'.format(
                 "No",
-                "Название магазина",
-                "Название продукта",
-                "Стоимость товара"
+                "Ф.И.О.",
+                "Должность",
+                "Год"
             )
         )
         print(line)
-        for idx, listshop in enumerate(list_shop, 1):
+
+        # Вывести данные о всех сотрудниках.
+        for idx, worker in enumerate(staff, 1):
             print(
-                '| {:>6} | {:<20} | {:<30} | {:>20} |'.format(
+                '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
                     idx,
-                    listshop.get('name_shop', ''),
-                    listshop.get('name_product', ''),
-                    listshop.get('prise', 0)
+                    worker.get('name', ''),
+                    worker.get('post', ''),
+                    worker.get('year', 0)
                 )
             )
         print(line)
     else:
-        print("Список магазинов пуст.")
+        print("Список работников пуст.")
 
 
-def select_product(list_shop, shop_sear):
+def select_workers(staff, period):
     """
-    Выбрать продукт.
+    Выбрать работников с заданным стажем.
     """
-    search_shop = []
-    for shop_sear_itme in list_shop:
-        if shop_sear == shop_sear_itme['name_product']:
-            search_shop.append(shop_sear_itme)
-    return search_shop
+
+    # Получить текущую дату.
+    today = date.today()
+
+    # Сформировать список работников.
+    result = []
+    for employee in staff:
+        if today.year - employee.get('year', today.year) >= period:
+            result.append(employee)
+
+        # Возвратить список выбранных работников.
+        return result
 
 
-def save_shop(file_name, list_shop):
+def save_workers(file_name, staff):
     """
     Сохранить всех работников в файл JSON.
     """
+    # Открыть файл с заданным именем для записи.
     with open(file_name, "w", encoding="utf-8") as fout:
-        json.dump(list_shop, fout, ensure_ascii=False, indent=4)
+        # Выполнить сериализацию данных в формат JSON.
+        # Для поддержки кирилицы установим ensure_ascii=False
+        json.dump(staff, fout, ensure_ascii=False, indent=4)
 
 
-def load_list_shop(file_name):
+def load_workers(file_name):
     """
     Загрузить всех работников из файла JSON.
     """
+    # Открыть файл с заданным именем для чтения.
     with open(file_name, "r", encoding="utf-8") as fin:
         return json.load(fin)
 
 
 def main(command_line=None):
+    # Создать родительский парсер для определения имени файла.
     file_parser = argparse.ArgumentParser(add_help=False)
     file_parser.add_argument(
         "filename",
         action="store",
         help="The data file name"
     )
+
+    # Создать основной парсер командной строки.
     parser = argparse.ArgumentParser("workers")
     parser.add_argument(
         "--version",
@@ -98,72 +116,85 @@ def main(command_line=None):
     )
     subparsers = parser.add_subparsers(dest="command")
 
+    # Создать субпарсер для добавления работника.
     add = subparsers.add_parser(
         "add",
         parents=[file_parser],
-        help="Add a new shop"
+        help="Add a new worker"
     )
     add.add_argument(
-        "-ns",
-        "--name_shop",
+        "-n",
+        "--name",
         action="store",
         required=True,
-        help="The shop's name"
+        help="The worker's name"
     )
     add.add_argument(
-        "-np",
-        "--name_product",
+        "-p",
+        "--post",
         action="store",
-        help="The product's name"
+        help="The worker's post"
     )
     add.add_argument(
-        "-ps",
-        "--prise",
+        "-y",
+        "--year",
         action="store",
         type=int,
         required=True,
-        help="Cost of goods"
+        help="The year of hiring"
     )
+    # Создать субпарсер для отображения всех работников.
     _ = subparsers.add_parser(
         "display",
         parents=[file_parser],
-        help="Display all shops"
+        help="Display all workers"
     )
+    # Создать субпарсер для выбора работников.
     select = subparsers.add_parser(
         "select",
         parents=[file_parser],
-        help="Select the product"
+        help="Select the workers"
     )
     select.add_argument(
-        "-SS",
-        "--shop_sear",
+        "-P",
+        "--period",
         action="store",
-        type=str,
+        type=int,
         required=True,
-        help="The name product"
+        help="The required period"
     )
+    # Выполнить разбор аргументов командной строки.
     args = parser.parse_args(command_line)
+
+    # Загрузить всех работников из файла, если файл существует.
     is_dirty = False
     if os.path.exists(args.filename):
-        shop = load_list_shop(args.filename)
+        workers = load_workers(args.filename)
     else:
-        shop = []
+        workers = []
+
+    # Добавить работника.
     if args.command == "add":
-        shop = add_shop(
-            shop,
-            args.name_shop,
-            args.name_product,
-            args.prise
+        workers = add_worker(
+            workers,
+            args.name,
+            args.post,
+            args.year
         )
         is_dirty = True
-    elif args.command == "display":
-        display_shop(shop)
 
+    # Отобразить всех работников.
+    elif args.command == "display":
+        display_workers(workers)
+
+    # Выбрать требуемых рааботников.
     elif args.command == "select":
-        selected = select_product(shop, args.shop_sear)
-        display_shop(selected)
+        selected = select_workers(workers, args.period)
+        display_workers(selected)
+
+    # Сохранить данные в файл, если список работников был изменен.
     if is_dirty:
-        save_shop(args.filename, shop)
+        save_workers(args.filename, workers)
 
 
 if __name__ == '__main__':
